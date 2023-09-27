7afe7930f30f983388945cad2489c22b69aa2e11
Juantrevi <juantrevi70@gmail.com>
2023-09-27 22:52:27 +0100

    HW-@RestController & @GetMapping @PathVariable - UsersDao

---
+++ src/main/java/com/webservices/restfulwebservice/service/UserDaoService.java
@@ -0,0 +1,28 @@
+package com.webservices.restfulwebservice.service;
+
+import com.webservices.restfulwebservice.entities.User;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDaoService {
+    //JPA/Hibernate to interact with database
+    private static List<User> users = new ArrayList<>();
+    static {
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Eve", LocalDate.now().minusYears(20)));
+        users.add(new User(3, "Jack", LocalDate.now().minusYears(10)));
+        users.add(new User(4, "Jill", LocalDate.now().minusYears(5)));
+        users.add(new User(5, "John", LocalDate.now().minusYears(19)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+
+}
