3eca3c7f958b6085ce1c61cf8cc16c560685616c
Juantrevi <juantrevi70@gmail.com>
2023-09-27 22:25:58 +0100

    HW-@RestController & @GetMapping @PathVariable

--- src/main/java/com/webservices/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/webservices/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.webservices.restfulwebservice.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //REST API
@@ -17,7 +18,12 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean() {
 
         return new HelloWorldBean("Hello World");
+    }
 
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+
+        return new HelloWorldBean("Hello World " + name);
     }
 
 }
