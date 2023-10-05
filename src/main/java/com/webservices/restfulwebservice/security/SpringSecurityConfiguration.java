97185d0f9df64cec1979c7d6920845629652feba
Juantrevi <juantrevi70@gmail.com>
2023-10-05 17:05:07 +0100

    Spring security 1.0.0

---
+++ src/main/java/com/webservices/restfulwebservice/security/SpringSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.webservices.restfulwebservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration{
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated());
+
+        http.httpBasic(Customizer.withDefaults());
+
+        //url to logout
+        http.logout(logout -> logout.logoutUrl("/logout"));
+
+        http.csrf().disable();
+
+        return http.build();
+    }
+
+}
